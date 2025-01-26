a4f50136a7777cc7d05931dba59b8e014f68d7ee
Alex Pellegrino <alex.pellegrino@itiscuneo.eu>
2025-01-26 10:32:10 +0100

    Aggiungere i file di progetto.

---
+++ ProgettoMeteo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgettoMeteo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
