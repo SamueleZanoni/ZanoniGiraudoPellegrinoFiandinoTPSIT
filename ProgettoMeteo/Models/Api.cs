e2c40de0aff236c768f016a6c04538187a7f0f33
Alex Pellegrino <alex.pellegrino@itiscuneo.eu>
2025-01-26 11:05:06 +0100

    prime due api: meteo corrente/trovaCittaDaNome

---
+++ ProgettoMeteo/Models/Api.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ProgettoMeteo.Models
+{
+    public class Api
+    {
+        public async Task<string> MeteoCorrente(float latitudine, float longitudine)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.openweathermap.org/data/2.5/weather?lat={latitudine}&lon={longitudine}&appid=bec2cee97778ae672a64740c7aa3657d");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> MeteoGeoLocation(string nomeCitta)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.openweathermap.org/geo/1.0/direct?q={nomeCitta}&limit=2&appid=bec2cee97778ae672a64740c7aa3657d");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();;
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+    }
+}
